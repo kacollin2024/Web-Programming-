e02b01cbbd92e1ac7ed056eb0a29eacea1a6ddb4
Kailey Collins Fiore <kacollin2024@students.curry.edu>
2025-03-27 13:36:29 -0400

    Add files via upload

---
+++ question3.java
@@ -0,0 +1,14 @@
+public class question3 {
+
+let Grade = 90;
+if (Grade >= 90){
+    alert("Grade A");
+    
+    
+    }
+    private void alert(String string) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'alert'");
+    }
+    
+}
